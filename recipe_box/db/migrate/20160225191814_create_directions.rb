8405e92927ad83fccff95d31d04f368695d8458c
Ludovic Boisnon <lboisnon@gmail.com>
2016-02-25 12:05:41 -0800

    Added recipes with ingredients and directions in nested form

---
+++ recipe_box/db/migrate/20160225191814_create_directions.rb
@@ -0,0 +1,11 @@
+class CreateDirections < ActiveRecord::Migration
+  def change
+    create_table :directions do |t|
+      t.string :step
+      t.string :text
+      t.belongs_to :recipe, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
